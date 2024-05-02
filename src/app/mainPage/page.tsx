e444aa0be68f6b141ce49b2cfac1137ae8208e28
sstlby <mlby11132020@outlook.com>
2024-05-03 03:07:20 +0800

    feat: 左边侧边栏设计

--- src/app/mainPage/page.tsx
+++ src/app/mainPage/page.tsx
@@ -3,17 +3,22 @@ import React from 'react';
 import { Col, Row } from 'antd';
 
 import HeadMenu from '@/components/HeadMenu';
+
+import LeftMenu from './components/LeftMenu';
+
 export default function mainPage() {
   return (
     <>
       <HeadMenu />
       {/* 主页栅格布局 */}
       <Row style={{ height: '94vh' }}>
-        <Col xs={{ span: 0 }} md={{ span: 6 }} lg={{ span: 4 }} style={{ backgroundColor: 'red' }}></Col>
+        <Col xs={{ span: 0 }} md={{ span: 5 }} lg={{ span: 5 }}>
+          <LeftMenu />
+        </Col>
         <Col
           xs={{ span: 24, offset: 0 }}
-          md={{ span: 17, offset: 1 }}
-          lg={{ span: 12, offset: 1 }}
+          md={{ span: 18, offset: 1 }}
+          lg={{ span: 12, offset: 0 }}
           style={{ backgroundColor: 'blue' }}
         ></Col>
         <Col
